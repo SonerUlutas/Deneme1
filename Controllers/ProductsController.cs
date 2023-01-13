16dc5dced8084d7016d3f293f08950389e174add
SonerUlutas <117598859+SonerUlutas@users.noreply.github.com>
2023-01-13 18:25:55 +0300

    Add files via upload

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("Products")]
+    public class ProductsController:ControllerBase
+    {
+        [Route("Merhaba")]
+        public string Merhaba()
+        {
+            return "Merhaba";
+        }
+        [Route("Hello")]
+        public string Hello()
+        {
+            return "Hello";
+        }
+    }
+}
